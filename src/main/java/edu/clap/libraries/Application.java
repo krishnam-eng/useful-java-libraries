56df2ad0df7e7ea2c9fab434b7ac5902fd7fb070
balamurugan <krishnam.balamurugan.eng@gmail.com>
2022-03-11 13:36:57 +0530

    package plceholder for useful lib

--- src/main/java/edu/clap/libraries/Application.java
+++ src/main/java/edu/clap/libraries/Application.java
@@ -1,4 +1,4 @@
-package edu.clap.usefuljavalibraries;
+package edu.clap.libraries;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/edu/clap/libraries/thirdparty/devutil/lombok/package-info.java
@@ -0,0 +1,1 @@
+package edu.clap.libraries.thirdparty.devutil.lombok;
